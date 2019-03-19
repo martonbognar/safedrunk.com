04259dd48a3ff144552c8feb6c41dbd0886bedc7
marton bognar <martonbognar@sch.bme.hu>
2019-03-19 15:56:30 +0100

    Add Google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,5 +91,13 @@
             </div>
         </footer>
     </div>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136505103-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-136505103-1');
+    </script>
 </body>
 </html>
