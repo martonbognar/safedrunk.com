4151bc8918001f565c3a9ffc6cb8e6d779ec52ec
marton bognar <martonbognar@sch.bme.hu>
2019-08-27 08:56:55 +0200

    Move API routes

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,30 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::get('/beverages', 'BeverageController@listCombined');
+Route::post('/beverages', 'BeverageController@store');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/beverages/own', 'BeverageController@listOwn');
+Route::get('/beverages/filter/{keyword}', 'BeverageController@listFiltered');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::patch('/beverages/{beverage}', 'BeverageController@update');
+Route::delete('/beverages/{beverage', 'BeverageController@destroy');
+
+Route::get('/beverages/pending', 'BeverageController@listPending');
+
+Route::get('/favorites', 'UserController@favorites');
+Route::post('/favorites', 'UserController@addFavorite');
+Route::delete('/favorites/{favorite}', 'UserController@removeFavorite');
+
+Route::get('/sessions', 'SessionController@list');
+Route::post('/sessions', 'SessionController@store');
+
+Route::get('/sessions/recent', 'SessionController@listRecent');
+
+Route::delete('/sessions/{session}', 'SessionController@destroy');
+
+Route::get('/sessions/{session}/drinks', 'DrinkController@index');
+Route::post('/sessions/{session}/drinks', 'DrinkController@store');
+Route::post('/sessions/{session}/drinks/{drink}/duplicate', 'DrinkController@duplicate');
+Route::delete('/sessions/{session}/drinks/{drink}', 'DrinkController@destroy');
+
+Route::get('/personal', 'UserController@personal');
