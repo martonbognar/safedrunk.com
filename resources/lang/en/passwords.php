0e5967dbfd828853cf53cabeb7b23183e459f13d
marton bognar <martonbognar@sch.bme.hu>
2020-02-21 15:52:09 +0100

    Add notice about spam folder

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -15,7 +15,7 @@ return [
 
     'password' => 'Passwords must be at least six characters and match the confirmation.',
     'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'sent' => 'We have e-mailed your password reset link! If you do not receive it shortly, make sure to check your spam folder!',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that e-mail address.",
 
